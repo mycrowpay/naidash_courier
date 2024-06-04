fb177681265e50586492f3477b900c256ecf53ae
Mike <19901759+wakandavibranium@users.noreply.github.com>
2024-06-05 00:16:15 +0300

    feat(controllers): add an endpoint for creating a stage

---
+++ __manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "NaiDash Courier Services",
+
+    'summary': "NaiDash Courier Management System",
+
+    'description': """
+        NaiDash is a Courier Management System
+    """,
+
+    'author': "NaiDash",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/17.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Customizations',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'courier_manage'],
+
+    # always loaded
+    'data': [
+        # 'security/groups.xml',
+        # 'security/ir.model.access.csv',
+        # 'data/ir_sequence_data.xml',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,    
+    'license': 'LGPL-3',        
+}
+

09b9c77fe648b1a47d85c3305cef2c200aa39dbf
Mike <19901759+wakandavibranium@users.noreply.github.com>
2025-04-27 12:40:44 +0300

    patch(models): calculate the price for each courier line item based on the product's price

--- models/courier_line.py
+++ models/courier_line.py
@@ -11,3 +11,14 @@ class NaidashCourierLine(models.Model):
     _inherit = "courier.line.custom"
     
     quantity = fields.Float(string = "Quantity", tracking=True)
+
+
+    @api.depends('product_id', 'quantity')
+    def _compute_courier_subtotal(self):
+        for rec in self:
+            rec.total_weight = 0
+            rec.volumetric_weight = 0
+            rec.total_volumetric_weight = 0
+            rec.weight_cost = 0
+            rec.volumetric_weight_cost = 0
+            rec.courier_subtotal = rec.product_id.lst_price * rec.quantity
